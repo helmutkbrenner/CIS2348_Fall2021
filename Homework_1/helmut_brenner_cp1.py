c601c081347d839305b6132cedce12e2c210e154
helmutkbrenner <66104915+helmutkbrenner@users.noreply.github.com>
2021-09-08 17:07:04 -0500

    almost finished, added the date time classes and then calculated the years
    Signed-off-by: helmutkbrenner <66104915+helmutkbrenner@users.noreply.github.com>

--- Homework_1/helmut_brenner_cp1.py
+++ Homework_1/helmut_brenner_cp1.py
@@ -1,14 +1,14 @@
 # Helmut Brenner #
 # 2037275 #
 
-import math
+import datetime
 
 if __name__ == '__main__':
     print('Birthday Calculator')
     print('Current Day')
 
     current_month = int(input('Current Month: '))
-    current_day = int(input('Current Day: '))
+    current_date = int(input('Current Day: '))
     current_year = int(input('Current Year: '))
 
     print('Next, What is your birthday?')
@@ -16,6 +16,17 @@ if __name__ == '__main__':
     user_b_month = int(input('Birth Month:'))
     user_b_day = int(input('Birthday:'))
     user_b_year = int(input('Birth Year:'))
+
+    current_day = datetime.date(current_year, current_month, current_date)
+    user_birthday = datetime.date(user_b_year, user_b_month, user_b_day)
+
+    time_delta = current_day - user_birthday
+    years_old = time_delta.days//365
+
+    if current_date == user_b_day & current_month == user_b_month:
+        print('Happy birthday, you are {} years old.'.format(years_old))
+    else:
+        print('You are {} years old!'.format(years_old))
 
 
 
