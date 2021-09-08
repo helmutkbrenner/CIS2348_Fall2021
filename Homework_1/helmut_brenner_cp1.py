3c06a1309d4ff4ebbc921cdee571f92fedf05def
helmutkbrenner <66104915+helmutkbrenner@users.noreply.github.com>
2021-09-08 15:08:35 -0500

    Add files via upload

---
+++ Homework_1/helmut_brenner_cp1.py
@@ -0,0 +1,26 @@
+# Helmut Brenner #
+# 2037275 #
+
+import math
+
+if __name__ == '__main__':
+    print('Birthday Calculator')
+    print('Current Day')
+
+    current_month = int(input('Current Month: '))
+    current_day = int(input('Current Day: '))
+    current_year = int(input('Current Year: '))
+
+    print('Next, What is your birthday?')
+
+    user_b_month = int(input('Birth Month:'))
+    user_b_day = int(input('Birthday:'))
+    user_b_year = int(input('Birth Year:'))
+
+
+
+
+
+
+
+
