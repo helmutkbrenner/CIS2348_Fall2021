e13bacbf9c15075d7df59d15d8a5ff1ff1488c83
helmutkbrenner <66104915+helmutkbrenner@users.noreply.github.com>
2021-09-13 12:51:25 -0500

    newline added and some forgotten "!!"
    Signed-off-by: helmutkbrenner <66104915+helmutkbrenner@users.noreply.github.com>

--- Homework_1/zybooks_1_20.py
+++ Homework_1/zybooks_1_20.py
@@ -2,13 +2,13 @@
 # 2037275 #
 
 if __name__ == '__main__':
-    user_num1 = int(input('Enter integer:'))
+    user_num1 = int(input('Enter integer:\n'))
 
     print('You entered: {}'.format(user_num1))
 
     print('{} squared is {}'.format(user_num1, (user_num1 * user_num1)))
 
-    print('And {} cubed is {}'.format(user_num1, (user_num1 * user_num1 * user_num1)))
+    print('And {} cubed is {} !!'.format(user_num1, (user_num1 * user_num1 * user_num1)))
 
     user_num2 = int(input('Enter another integer:'))
 
