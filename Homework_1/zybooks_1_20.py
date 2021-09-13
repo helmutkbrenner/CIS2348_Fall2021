d5b976f7035aeb23cc22ede4d5f0b43ff1d0b41e
helmutkbrenner <66104915+helmutkbrenner@users.noreply.github.com>
2021-09-13 12:46:51 -0500

    did the assignment by writing print statements.I may remove line 4 if zybooks doesn't like it for submissions
    Signed-off-by: helmutkbrenner <66104915+helmutkbrenner@users.noreply.github.com>

--- Homework_1/zybooks_1_20.py
+++ Homework_1/zybooks_1_20.py
@@ -1,2 +1,17 @@
 # Helmut brenner #
 # 2037275 #
+
+if __name__ == '__main__':
+    user_num1 = int(input('Enter integer:'))
+
+    print('You entered: {}'.format(user_num1))
+
+    print('{} squared is {}'.format(user_num1, (user_num1 * user_num1)))
+
+    print('And {} cubed is {}'.format(user_num1, (user_num1 * user_num1 * user_num1)))
+
+    user_num2 = int(input('Enter another integer:'))
+
+    print('{} + {} is {}'.format(user_num1, user_num2, (user_num1 + user_num2)))
+
+    print('{} * {} is {}'.format(user_num1, user_num2, (user_num1 * user_num2)))
