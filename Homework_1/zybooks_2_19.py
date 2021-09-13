8c5761f74c8da4b67098a3779d988d3506643e5c
helmutkbrenner <66104915+helmutkbrenner@users.noreply.github.com>
2021-09-13 13:33:06 -0500

    fixed spacing and text issue
    Signed-off-by: helmutkbrenner <66104915+helmutkbrenner@users.noreply.github.com>

--- Homework_1/zybooks_2_19.py
+++ Homework_1/zybooks_2_19.py
@@ -4,7 +4,7 @@
 if __name__ == '__main__':
     #  First thing, get user input. Decided to use floats on everything for decimal accuracy.
 
-    cupslemjuice = float(input('Enter the amount of lemon juice (in cups):\n'))
+    cupslemjuice = float(input('Enter amount of lemon juice (in cups):\n'))
     cupswater = float(input('Enter amount of water (in cups):\n'))
     cupsagave = float(input('Enter amount of agave nectar (in cups):\n'))
     numservings = float(input('How many servings does this make?\n'))
@@ -14,7 +14,7 @@ if __name__ == '__main__':
     print('Lemonade ingredients - yields {:.2f} servings'.format(numservings))
     print('{:.2f} cup(s) lemon juice'.format(cupslemjuice))
     print('{:.2f} cup(s) water'.format(cupswater))
-    print('{:.2f} cup(s) agave nectar'.format(cupsagave))
+    print('{:.2f} cup(s) agave nectar\n'.format(cupsagave))
 
     # Get desired number of servings
 
