787fb56d6076e8cde9a1e2f3bef287f4a5c8e275
helmutkbrenner <66104915+helmutkbrenner@users.noreply.github.com>
2021-09-14 13:39:42 -0500

    added spaces and got rid of warning messages
    Signed-off-by: helmutkbrenner <66104915+helmutkbrenner@users.noreply.github.com>

--- Homework_1/zybooks_3_19.py
+++ Homework_1/zybooks_3_19.py
@@ -19,11 +19,12 @@ if __name__ == '__main__':
     print('Wall area: {} square feet'.format(wall_area))
 
     # FIXME (2): Calculate and output the amount of paint in gallons needed to paint the wall
-    print('Paint needed: {:.2f} gallons'.format(wall_area / 350 ))
+    print('Paint needed: {:.2f} gallons'.format(wall_area / 350))
 
-    # FIXME (3): Calculate and output the number of 1 gallon cans needed to paint the wall, rounded up to nearest integer
+    # FIXME (3): Calculate/output the number of 1 gallon cans needed to paint the wall, rounded up to nearest integer
     print('Cans needed: {} can(s)\n'.format(math.ceil(wall_area / 350)))
 
     # FIXME (4): Calculate and output the total cost of paint can needed depending on color
     user_color = input('Choose a color to paint the wall:\n')
-    print('Cost of purchasing {} paint: ${}'.format(user_color, paint_colors[user_color] * (math.ceil(wall_area / 350))))
+    print('Cost of purchasing {} paint: ${}'.format(user_color,  # this indentation pattern is pycharms doing.
+                                                    paint_colors[user_color] * (math.ceil(wall_area / 350))))
