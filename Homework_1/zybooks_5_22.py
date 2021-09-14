891fb0174351f77511c4e9eadbb620674e0efe16
helmutkbrenner <66104915+helmutkbrenner@users.noreply.github.com>
2021-09-14 14:35:00 -0500

    Did a good bit of work figuring out the conditional, but in the end it works for all realistic cases
    Signed-off-by: helmutkbrenner <66104915+helmutkbrenner@users.noreply.github.com>

--- Homework_1/zybooks_5_22.py
+++ Homework_1/zybooks_5_22.py
@@ -2,3 +2,38 @@
 # 2037275 #
 
 if __name__ == '__main__':
+
+    # Dictionary is a good choice for holding this type of correlated data.
+    services = {
+        'Oil change': 35,
+        'Tire rotation': 19,
+        'Car wash': 7,
+        'Car wax': 12,
+        '-': 'No service'
+    }
+
+    # Normally would not use this many print statements but we have not learned loops yet...
+    print('Davy\'s auto shop services')
+    print('Oil change -- ${}'.format(services['Oil change']))
+    print('Tire rotation -- ${}'.format(services['Tire rotation']))
+    print('Car wash -- ${}'.format(services['Car wash']))
+    print('Car wax -- ${}\n'.format(services['Car wax']))
+
+    # Collect the desired services from user.
+    service1 = str(input('Select first service:\n'))
+    service2 = str(input('Select second service:\n'))
+
+    # printing/calculating  the invoice using the dictionary and if statements to check for 'No service' option
+    print('\nDavy\'s auto shop invoice\n')
+    if service1 == '-':
+        print('Service 1: {}'.format(service1))
+        print('Service 2: {}, ${}'.format(service2, services[service2]))
+        print('\nTotal: ${}'.format(services[service2]))
+    elif service2 == '-':
+        print('Service 1: {}, ${}'.format(service1, services[service1]))
+        print('Service 2: {}'.format(service1))
+        print('\nTotal: ${}'.format(services[service1]))
+    else:
+        print('Service 1: {}, ${}'.format(service1, services[service1]))
+        print('Service 2: {}, ${}'.format(service2, services[service2]))
+        print('\nTotal: ${}'.format(services[service1] + services[service2]))
