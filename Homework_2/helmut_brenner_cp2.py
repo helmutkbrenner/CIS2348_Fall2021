aef557f708522a642948789e733f4550ad8d63ba
helmutkbrenner <66104915+helmutkbrenner@users.noreply.github.com>
2021-10-08 11:02:07 -0500

    The code will now filter all the incorrect formats except for dates that are after the current date
    Signed-off-by: helmutkbrenner <66104915+helmutkbrenner@users.noreply.github.com>

--- Homework_2/helmut_brenner_cp2.py
+++ Homework_2/helmut_brenner_cp2.py
@@ -4,10 +4,16 @@
 import datetime
 
 if __name__ == '__main__':
+    months_list = ['January','February','March','April','May','June','July','August','September','October',
+                   'November','December']
     # this block takes user input line by line and loads it into a list of strings.
     line_list = []
     user_line_input = ''
     while user_line_input != '-1':
-        user_line_input = input()
-        line_list.append(user_line_input)
+        user_line_input = str(input())
+        # these next statements check for incorrect date formats. I understand this is probably not the best way so
+        # I will continue to think about iterative improvements.
+        if user_line_input.count(' ') == 2 and user_line_input.count(',') == 1 and user_line_input.find('.') == -1:
+            line_list.append(user_line_input)
     print(line_list)
+    # This next block of code will parse the dates ito a datetime object
