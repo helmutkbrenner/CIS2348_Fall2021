e785de7aca423ee34fd0b24f6621fbc2ac13c370
helmutkbrenner <66104915+helmutkbrenner@users.noreply.github.com>
2021-10-08 15:07:23 -0500

    fixed white spacing
    Signed-off-by: helmutkbrenner <66104915+helmutkbrenner@users.noreply.github.com>

--- Homework_2/zybooks_6_22.py
+++ Homework_2/zybooks_6_22.py
@@ -16,7 +16,7 @@ if __name__ == '__main__':
             answer1 = (int_A1 * x) + (int_B1 * y)
             answer2 = (int_A2 * x) + (int_B2 * y)
             if answer1 == int_C1 and answer2 == int_C2:
-                print(x, ' ', y)
+                print(x, y)
                 has_answer = True
     if not has_answer:
         print('No solution')
