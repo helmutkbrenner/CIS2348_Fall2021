6709e3d5f88d14b4c96cdd546acc3f544d2f7e85
helmutkbrenner <66104915+helmutkbrenner@users.noreply.github.com>
2021-10-08 14:54:10 -0500

    still does not work, math seems fine buuuut
    Signed-off-by: helmutkbrenner <66104915+helmutkbrenner@users.noreply.github.com>

--- Homework_2/zybooks_6_22.py
+++ Homework_2/zybooks_6_22.py
@@ -12,6 +12,7 @@ if __name__ == '__main__':
 
     for x in range(-10, 11):
         for y in range(-10, 11):
-            if (int_A1*x) + (int_B2*y) == int_C1 and (int_A2*x) - (int_B2*y) == int_C2:
+            if ((int_A1 * x) + (int_B1 * y) == int_C1) and ((int_A2 * x) - (int_B2 * y) == int_C2):
                 print(x, ' ', y)
+                break
     print('No solution')
