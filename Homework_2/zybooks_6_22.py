08d84f16c14df7e7f1b7cc8d142f7a51df63214e
helmutkbrenner <66104915+helmutkbrenner@users.noreply.github.com>
2021-10-08 14:45:00 -0500

    Does not work currently, but the structure is there.
    Signed-off-by: helmutkbrenner <66104915+helmutkbrenner@users.noreply.github.com>

--- Homework_2/zybooks_6_22.py
+++ Homework_2/zybooks_6_22.py
@@ -1,2 +1,17 @@
 # Helmut Brenner #
 # 2037275 #
+
+if __name__ == '__main__':
+    int_A1 = int(input())
+    int_B1 = int(input())
+    int_C1 = int(input())
+
+    int_A2 = int(input())
+    int_B2 = int(input())
+    int_C2 = int(input())
+
+    for x in range(-10, 11):
+        for y in range(-10, 11):
+            if (int_A1*x) + (int_B2*y) == int_C1 and (int_A2*x) - (int_B2*y) == int_C2:
+                print(x, ' ', y)
+    print('No solution')
