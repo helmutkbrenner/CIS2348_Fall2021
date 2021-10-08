2612ce2760cb1537d6397f929dba1a3bd491901a
helmutkbrenner <66104915+helmutkbrenner@users.noreply.github.com>
2021-10-08 15:06:37 -0500

    I think I got it too work. I was just trying to do too much in a single expression
    Signed-off-by: helmutkbrenner <66104915+helmutkbrenner@users.noreply.github.com>

--- Homework_2/zybooks_6_22.py
+++ Homework_2/zybooks_6_22.py
@@ -9,10 +9,14 @@ if __name__ == '__main__':
     int_A2 = int(input())
     int_B2 = int(input())
     int_C2 = int(input())
+    has_answer = False
 
     for x in range(-10, 11):
         for y in range(-10, 11):
-            if ((int_A1 * x) + (int_B1 * y) == int_C1) and ((int_A2 * x) - (int_B2 * y) == int_C2):
+            answer1 = (int_A1 * x) + (int_B1 * y)
+            answer2 = (int_A2 * x) + (int_B2 * y)
+            if answer1 == int_C1 and answer2 == int_C2:
                 print(x, ' ', y)
-                break
-    print('No solution')
+                has_answer = True
+    if not has_answer:
+        print('No solution')
