7dbcc3ee49a06596ed4180a5547c0948266f45da
helmutkbrenner <66104915+helmutkbrenner@users.noreply.github.com>
2021-10-08 10:20:57 -0500

    wrote block of code to take dates and load into list
    Signed-off-by: helmutkbrenner <66104915+helmutkbrenner@users.noreply.github.com>

--- Homework_2/helmut_brenner_cp2.py
+++ Homework_2/helmut_brenner_cp2.py
@@ -1,2 +1,13 @@
 # Helmut Brenner # 
 # 2037275 #
+
+import datetime
+
+if __name__ == '__main__':
+    # this block takes user input line by line and loads it into a list of strings.
+    line_list = []
+    user_line_input = ''
+    while user_line_input != '-1':
+        user_line_input = input()
+        line_list.append(user_line_input)
+    print(line_list)
