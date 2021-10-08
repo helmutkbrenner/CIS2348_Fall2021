284778b9f5fe0eeeeae93bfab75f2a00b6ae9a52
helmutkbrenner <66104915+helmutkbrenner@users.noreply.github.com>
2021-10-08 14:17:22 -0500

    Everything works and is notated as necessary, pretty straight forward though.
    Signed-off-by: helmutkbrenner <66104915+helmutkbrenner@users.noreply.github.com>

--- Homework_2/zybooks_6_17.py
+++ Homework_2/zybooks_6_17.py
@@ -1,2 +1,18 @@
 # Helmut Brenner #
 # 2037275 #
+
+if __name__ == '__main__':
+    # Made a dictionary with key value pairs
+    cipher = {'i': '!', 'a': '@', 'm': 'M', 'B': '8', 'o': '.'}
+    appendix = 'q*s'
+
+    user_pass = str(input())
+    strong_pass = ''
+    # constructs a new string while referencing the dictionary.
+    for i in range(len(user_pass)):
+        if user_pass[i] in cipher:
+            strong_pass += cipher[user_pass[i]]
+        else:
+            strong_pass += user_pass[i]
+    strong_pass += appendix
+    print(strong_pass)
