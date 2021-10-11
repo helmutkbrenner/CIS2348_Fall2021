8b6d907ab1f64c2d4b97799d76fac634a9a5d71e
helmutkbrenner <66104915+helmutkbrenner@users.noreply.github.com>
2021-10-11 15:04:54 -0500

    ok holy moly, finally got the csv file system working, and iterating through the words. Once I got that figured out all it took was a simple if statement to throw them in a dictionary or add to a tally
    Signed-off-by: helmutkbrenner <66104915+helmutkbrenner@users.noreply.github.com>

--- Homework_2/zybooks_9_10.py
+++ Homework_2/zybooks_9_10.py
@@ -1,2 +1,17 @@
 # Helmut Brenner #
 # 2037275 #
+
+import csv
+
+if __name__ == '__main__':
+    word_counts = {}
+    with open('input1.csv', 'r') as input1:
+        data_line = csv.reader(input1, delimiter=',')
+        for row in data_line:
+            for word in row:
+                if word not in word_counts:
+                    word_counts[word] = 1
+                else:
+                    word_counts[word] += 1
+
+    print(word_counts)
