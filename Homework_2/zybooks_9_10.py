0024d35b29e2c9777d649ed64b0db5ef480065d0
helmutkbrenner <66104915+helmutkbrenner@users.noreply.github.com>
2021-10-11 15:14:43 -0500

    everything works, and I added user input ability, plus notes.
    Signed-off-by: helmutkbrenner <66104915+helmutkbrenner@users.noreply.github.com>

--- Homework_2/zybooks_9_10.py
+++ Homework_2/zybooks_9_10.py
@@ -4,8 +4,11 @@
 import csv
 
 if __name__ == '__main__':
+    user_input = str(input())
     word_counts = {}
-    with open('input1.csv', 'r') as input1:
+    #  Used a with statement to ensure file close. Then iterate through the row and words in the data_line.
+    #  then an if statement increments values or adds a key.
+    with open(user_input, 'r') as input1:
         data_line = csv.reader(input1, delimiter=',')
         for row in data_line:
             for word in row:
@@ -14,4 +17,6 @@ if __name__ == '__main__':
                 else:
                     word_counts[word] += 1
 
-    print(word_counts)
+    #  prints the dictionary with formatting
+    for key in word_counts:
+        print('{} {}'.format(key, word_counts[key]))
