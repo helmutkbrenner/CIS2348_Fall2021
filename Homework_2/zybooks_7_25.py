9fec8adfabd01e26d950bde4e666d523649f9094
helmutkbrenner <66104915+helmutkbrenner@users.noreply.github.com>
2021-10-11 13:15:32 -0500

    Passes all the tests and outputs as desired. Functions could probably be streamlined in the future, probably no need to intake 5 separate numbers in the print function, better to take a list.
    Signed-off-by: helmutkbrenner <66104915+helmutkbrenner@users.noreply.github.com>

--- Homework_2/zybooks_7_25.py
+++ Homework_2/zybooks_7_25.py
@@ -2,6 +2,7 @@
 # 2037275 #
 
 def exact_change(number):
+    #  This function uses the modulo operator to find the max number of coins given a total
     num_dollars, num_quarters, num_dimes, num_nickels, num_pennies = 0, 0, 0, 0, 0
     running_total = number
 
@@ -23,10 +24,35 @@ def exact_change(number):
     return num_dollars, num_quarters, num_dimes, num_nickels, num_pennies
 
 
+def print_nums(number_1, number_2, number_3, number_4, number_5):
+    #  This function prints the number of coins and accounts for zero and plural situations
+    if number_1 > 1:
+        print('{} dollars'.format(number_1))
+    elif number_1 == 1:
+        print('{} dollar'.format(number_1))
+    if number_2 > 1:
+        print('{} quarters'.format(number_2))
+    elif number_2 == 1:
+        print('{} quarter'.format(number_2))
+    if number_3 > 1:
+        print('{} dimes'.format(number_3))
+    elif number_3 == 1:
+        print('{} dime'.format(number_3))
+    if number_4 > 1:
+        print('{} nickels'.format(number_4))
+    elif number_4 == 1:
+        print('{} nickel'.format(number_4))
+    if number_5 > 1:
+        print('{} pennies'.format(number_5))
+    elif number_5 == 1:
+        print('{} penny'.format(number_5))
+    return 0
+
+
+#  This is the main function which gets input and then uses the functions to calculate and display the information
 if __name__ == '__main__':
     user_input = float(input())
     if user_input <= 0:
         print('no change')
-    end_dollars, end_quarters, end_dimes, end_nickels, end_pennies = exact_change(user_input)
-
-
+    coin_list = exact_change(user_input)
+    print_nums(coin_list[0], coin_list[1], coin_list[2], coin_list[3], coin_list[4])
