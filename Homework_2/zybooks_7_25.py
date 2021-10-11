c3414d374fe50698ecd316706b96536733de1f76
helmutkbrenner <66104915+helmutkbrenner@users.noreply.github.com>
2021-10-11 12:22:02 -0500

    took out print statements added zero/negative detection still need to add correct print statements
    Signed-off-by: helmutkbrenner <66104915+helmutkbrenner@users.noreply.github.com>

--- Homework_2/zybooks_7_25.py
+++ Homework_2/zybooks_7_25.py
@@ -5,30 +5,26 @@ def exact_change(number):
     num_dollars, num_quarters, num_dimes, num_nickels, num_pennies = 0, 0, 0, 0, 0
     running_total = number
 
-    while running_total >= 1.00:
-        running_total -= 1.00
+    while running_total >= 100:
+        running_total -= 100
         num_dollars += 1
-    print(running_total)
-    while running_total >= .25:
-        running_total -= .25
+    while running_total >= 25:
+        running_total -= 25
         num_quarters += 1
-    print(running_total)
-    while running_total >= .10:
-        running_total -= .10
+    while running_total >= 10:
+        running_total -= 10
         num_dimes += 1
-    print(running_total)
-    while running_total >= .05:
-        running_total -= .05
+    while running_total >= 5:
+        running_total -= 5
         num_nickels += 1
-    print(running_total)
-    while running_total >= .01:
-        running_total -= .01
+    while running_total >= 1:
+        running_total -= 1
         num_pennies += 1
-    print(running_total)
-    print(num_dollars, num_quarters, num_dimes, num_nickels, num_pennies+1)
     return num_dollars, num_quarters, num_dimes, num_nickels, num_pennies+1
 
 
 if __name__ == '__main__':
     user_input = float(input())
+    if user_input <= 0:
+        print('no change')
     exact_change(user_input)
