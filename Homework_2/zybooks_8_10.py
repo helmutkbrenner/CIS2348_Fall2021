741fee4a1bd02cdedfe061d2a852e412370ee6cb
helmutkbrenner <66104915+helmutkbrenner@users.noreply.github.com>
2021-10-11 13:54:22 -0500

    seems functional without the test print statements. will see what zybooks says
    Signed-off-by: helmutkbrenner <66104915+helmutkbrenner@users.noreply.github.com>

--- Homework_2/zybooks_8_10.py
+++ Homework_2/zybooks_8_10.py
@@ -1,2 +1,27 @@
 # Helmut Brenner #
 # 2037275 #
+
+def palindrome_checker(word):
+    #  This function takes the string characters and loads them into a list, splice/reverses them and compares.
+    #  It then returns a boolean indicator.
+    char_list = []
+    palindrome = False
+    for i in range(len(word)):
+        if word[i] != ' ':
+            char_list.append(word[i])
+
+    rev_char_list = char_list[::-1]
+
+    if rev_char_list == char_list:
+        palindrome = True
+
+    return palindrome
+
+
+if __name__ == '__main__':
+    #  main function calls the function and prints the statement
+    user_word = str(input())
+    if palindrome_checker(user_word):
+        print('{} is a palindrome'.format(user_word))
+    else:
+        print('{} is not a palindrome'.format(user_word))
