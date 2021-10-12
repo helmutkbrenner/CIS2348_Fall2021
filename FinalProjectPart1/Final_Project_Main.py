51a6945da6a5a71c812257e702e44836bca5c01a
helmutkbrenner <66104915+helmutkbrenner@users.noreply.github.com>
2021-10-12 17:11:09 -0500

    cleaned up the code, made my notes more readable and succinct, compressed my test code so it wasn't so ugly and actually figured It wasnt showing all of my info 5-7 (thought my code was broken for a second there)
    Signed-off-by: helmutkbrenner <66104915+helmutkbrenner@users.noreply.github.com>

--- FinalProjectPart1/Final_Project_Main.py
+++ FinalProjectPart1/Final_Project_Main.py
@@ -20,38 +20,33 @@ class Item:
 
 if __name__ == '__main__':
     with open('ManufacturerList.csv', 'r') as manufacturer_file:
+        #  Opens the file and writes the contents to our class object Item, outputs to a master_item_list
         line_reader = csv.reader(manufacturer_file, delimiter=',')
         line_count = 0
-        master_item_list = []  # fill a list with the custom class objects?
+        master_item_list = []
         for row in line_reader:
             master_item_list.append(Item(row[0], row[1], row[2], row[3]))
             line_count += 1
-    print(master_item_list[0].item_id, master_item_list[0].manufacturer, master_item_list[0].item_type,
-          master_item_list[0].damaged)
-    print(master_item_list[1].item_id, master_item_list[1].manufacturer, master_item_list[1].item_type,
-          master_item_list[1].damaged)
-    print(master_item_list[2].item_id, master_item_list[2].manufacturer, master_item_list[2].item_type,
-          master_item_list[2].damaged)
-    print(master_item_list[3].item_id, master_item_list[3].manufacturer, master_item_list[3].item_type,
-          master_item_list[3].damaged)
-    print(master_item_list[4].item_id, master_item_list[4].manufacturer, master_item_list[4].item_type,
-          master_item_list[4].damaged)
+
+    for i in range(7):
+        #  TEST should produce the list as is
+        print(master_item_list[i].item_id, master_item_list[i].manufacturer, master_item_list[i].item_type,
+              master_item_list[i].damaged)
     print('\n')
+
+    #  This bit of code imports a special Operator function to reverse the lists according to common attributes
     from operator import attrgetter
-    master_item_list = sorted(master_item_list, key=attrgetter('manufacturer'))
+    sorted_by_manufacturer = sorted(master_item_list, key=attrgetter('manufacturer'))
     sorted_by_id = sorted(master_item_list, key=attrgetter('item_id'))
 
-    print(master_item_list[0].item_id, master_item_list[0].manufacturer, master_item_list[0].item_type,
-          master_item_list[0].damaged)
-    print(master_item_list[1].item_id, master_item_list[1].manufacturer, master_item_list[1].item_type,
-          master_item_list[1].damaged)
-    print(master_item_list[2].item_id, master_item_list[2].manufacturer, master_item_list[2].item_type,
-          master_item_list[2].damaged)
-    print(master_item_list[3].item_id, master_item_list[3].manufacturer, master_item_list[3].item_type,
-          master_item_list[3].damaged)
-    print(master_item_list[4].item_id, master_item_list[4].manufacturer, master_item_list[4].item_type,
-          master_item_list[4].damaged)
+    for i in range(7):
+        #  TEST should produce the list sorted by manufacturer
+        print(sorted_by_manufacturer[i].item_id, sorted_by_manufacturer[i].manufacturer,
+              sorted_by_manufacturer[i].item_type, sorted_by_manufacturer[i].damaged)
+
     print('\n')
 
-    for i in range(5):
-        print(sorted_by_id[i].item_id)
+    for i in range(7):
+        #  TEST should produce the list sorted by id
+        print(sorted_by_id[i].item_id, sorted_by_id[i].manufacturer, sorted_by_id[i].item_type, sorted_by_id[i].damaged)
+
