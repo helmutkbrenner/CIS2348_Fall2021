9bba287d936cf4603ba9f44cd30606595cf82695
helmutkbrenner <66104915+helmutkbrenner@users.noreply.github.com>
2021-10-12 15:46:32 -0500

    absolutely thrilled this worked sorta on the first try. Have the print statements in there to confirm that the list is filled with the objects and the class attributes are accessed by indexing the list.
    Signed-off-by: helmutkbrenner <66104915+helmutkbrenner@users.noreply.github.com>

--- FinalProjectPart1/Final_Project_Main.py
+++ FinalProjectPart1/Final_Project_Main.py
@@ -3,8 +3,9 @@
 
 import csv
 
-##  Making an item class with the following attributes and methods:
+
 class Item:
+    #  Making an item class with the following attributes and methods:
     def __init__(self, item_id, manufacturer, item_type, damaged='undamaged', price=0, service_date=''):
         self.item_id = item_id
         self.manufacturer = manufacturer
@@ -15,9 +16,15 @@ class Item:
 
 
 if __name__ == '__main__':
-    with open('ManufacturersList.csv', 'r') as manufacturerfile:
-        line_reader = csv.reader(manufacturerfile, delimiter=',')
-        line_count= 0
-        mega_item_list = []  ## fill a list with the custom class objects?
-        for row in line_reader????:
-            mega_item_list[line_count] = Item(row[0],row[1],row[2], row[3])
+    with open('ManufacturerList.csv', 'r') as manufacturer_file:
+        line_reader = csv.reader(manufacturer_file, delimiter=',')
+        line_count = 0
+        master_item_list = []  # fill a list with the custom class objects?
+        for row in line_reader:
+            master_item_list.append(Item(row[0], row[1], row[2]))
+            line_count += 1
+    print(master_item_list[0].item_id, master_item_list[0].manufacturer, master_item_list[0].item_type)
+    print(master_item_list[1].item_id, master_item_list[1].manufacturer, master_item_list[1].item_type)
+    print(master_item_list[2].item_id, master_item_list[2].manufacturer, master_item_list[2].item_type)
+    print(master_item_list[3].item_id, master_item_list[3].manufacturer, master_item_list[3].item_type)
+    print(master_item_list[4].item_id, master_item_list[4].manufacturer, master_item_list[4].item_type)
