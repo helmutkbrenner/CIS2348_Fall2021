ffcfd58d6e1a7cdde28ea0076b31c7b386d048fd
helmutkbrenner <66104915+helmutkbrenner@users.noreply.github.com>
2021-10-12 18:49:09 -0500

    worked on this function for QUITE a while to get it to work how I want. I m pretty sure it does but only further implementation will tell. Once I understood that I needed to use a while loop because I wasn't sure how long it would search, everything fell into place. I feel like it might be bad design to put a while loop in a for loop butttt, it works.
    Also added a test print case to check for proper assignment of prices.
    
    Signed-off-by: helmutkbrenner <66104915+helmutkbrenner@users.noreply.github.com>

--- FinalProjectPart1/Final_Project_Main.py
+++ FinalProjectPart1/Final_Project_Main.py
@@ -18,6 +18,16 @@ class Item:
         return repr((sel.item_id, self.manufacturer, self.item_type, self.damaged))
 
 
+def add_prices(master_list, price_pairs):
+    # This function is used to take the list of class objects and "append" the price onto the correct item entry
+    for k in range(len(master_list)):
+        j = 0
+        while master_list[k].item_id != price_pairs[j][0]:
+            j += 1
+        master_list[k].price = price_pairs[j][1]
+    return master_list
+
+
 if __name__ == '__main__':
     with open('ManufacturerList.csv', 'r') as manufacturer_file:
         #  Opens the file and writes the contents to our class object Item, outputs to a master_item_list
@@ -35,14 +45,16 @@ if __name__ == '__main__':
     print('\n')
 
     with open('PriceList.csv', 'r') as price_list_file:
-        line_reader1 = csv.reader(manufacturer_file, delimiter=',')
+        line_reader1 = csv.reader(price_list_file, delimiter=',')
         line_count1 = 0
         list_of_prices = []
         for row in line_reader1:
             list_of_prices.append([row[0], row[1]])
+            line_count1 += 1
 
     for i in range(7):
         print(list_of_prices[i][0], list_of_prices[i][1])
+    print('\n')
 
     #  This bit of code imports a special Operator function to reverse the lists according to common attributes
     from operator import attrgetter
@@ -59,3 +71,11 @@ if __name__ == '__main__':
     for i in range(7):
         #  TEST should produce the list sorted by id
         print(sorted_by_id[i].item_id, sorted_by_id[i].manufacturer, sorted_by_id[i].item_type, sorted_by_id[i].damaged)
+    print('\n')
+
+    add_prices(sorted_by_id, list_of_prices)
+
+    for i in range(7):
+        #  TEST should produce the list sorted by id
+        print(sorted_by_id[i].item_id, sorted_by_id[i].manufacturer, sorted_by_id[i].item_type, sorted_by_id[i].damaged,
+              sorted_by_id[i].price)
