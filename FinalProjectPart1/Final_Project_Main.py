e61616d669aa0f25be8ad702a4c6f32e5f68aae7
helmutkbrenner <66104915+helmutkbrenner@users.noreply.github.com>
2021-10-20 16:53:52 -0500

    had to run some tests to confirm that they are separate lists and are ordered correctly, now to experiment with writing to a csv file.
    Signed-off-by: helmutkbrenner <66104915+helmutkbrenner@users.noreply.github.com>

--- FinalProjectPart1/Final_Project_Main.py
+++ FinalProjectPart1/Final_Project_Main.py
@@ -98,3 +98,14 @@ if __name__ == '__main__':
     add_service_dates(sorted_by_id, service_dates_list)
 
     print_tester(sorted_by_id)
+
+    # FullInventory.csv -- all the items listed by row with all their information . The items
+    # should be sorted alphabetically by manufacturer. Each row should contain item ID,
+    # manufacturer name, item type, price, service date, and list if it is damaged. The item
+    # attributes must appear in this order.
+
+    print(sorted_by_manufacturer is sorted_by_id)
+    print_tester(master_item_list)
+    print_tester(sorted_by_manufacturer)
+    print_tester(sorted_by_id)
+
