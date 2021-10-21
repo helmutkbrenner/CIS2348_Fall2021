4760fab58ee524e75cb5c8d6072cc507254de677
helmutkbrenner <66104915+helmutkbrenner@users.noreply.github.com>
2021-10-20 19:16:34 -0500

    Mostly just re-arranged some code to make more logical sense and also added back the specifications, very useful being able to build off of them. And not so annoying to leave in now I know you can collapse large sections of code whenever
    Signed-off-by: helmutkbrenner <66104915+helmutkbrenner@users.noreply.github.com>

--- FinalProjectPart1/Final_Project_Main.py
+++ FinalProjectPart1/Final_Project_Main.py
@@ -84,18 +84,25 @@ if __name__ == '__main__':
             service_dates_list.append([row[0], row[1]])
             line_count2 += 1
 
+    #  Adds the prices to the correct items in their respective object classes
+    add_prices(master_item_list, list_of_prices)
+
+    #  Adds the service dates to the correct items in their respective object classes
+    add_service_dates(master_item_list, service_dates_list)
+
     #  This bit of code imports a special Operator function to sort the lists according to common attributes
     #  It even allows me to do two tier sorting, AMAZING.
+
     from operator import attrgetter
     sorted_by_manufacturer = sorted(master_item_list, key=attrgetter('manufacturer'))
     sorted_by_id = sorted(master_item_list, key=attrgetter('item_id'))
     sorted_by_type = sorted(master_item_list, key=attrgetter('item_type', 'item_id'))
+    sorted_by_service_date = sorted(master_item_list, key=attrgetter('service_date'))
 
-    #  Adds the prices to the correct items in their respective object classes
-    add_prices(sorted_by_id, list_of_prices)
-
-    #  Adds the service dates to the correct items in their respective object classes
-    add_service_dates(sorted_by_id, service_dates_list)
+    # a. FullInventory.csv -- all the items listed by row with all their information . The items
+    # should be sorted alphabetically by manufacturer. Each row should contain item ID,
+    # manufacturer name, item type, price, service date, and list if it is damaged. The item
+    # attributes must appear in this order.
 
     with open('FullInventory.csv', 'w') as full_inventory:
         #  This piece of code creates the file FullInventory and writes the data in the required order.
@@ -105,6 +112,11 @@ if __name__ == '__main__':
             line_writer.writerow([sorted_by_manufacturer[i].item_id, sorted_by_manufacturer[i].manufacturer,
                                   sorted_by_manufacturer[i].item_type, sorted_by_manufacturer[i].price,
                                   sorted_by_manufacturer[i].service_date, sorted_by_manufacturer[i].damaged])
+
+    # b. Item type Inventory list, i.e LaptopInventory.csv -- there should be a file for each item
+    # type and the item type needs to be in the file name. Each row of the file should contain
+    # item ID, manufacturer name, price, service date, and list if it is damaged. The items
+    # should be sorted by their item ID.
 
     #  One list provides the formatting names for the file names, and the other provides a reference for all types
     #  encountered in the input files. Allows for scalability for any number of item types... Theoretically.
@@ -122,6 +134,11 @@ if __name__ == '__main__':
                     line_writer2.writerow([sorted_by_type[q].item_id, sorted_by_type[q].manufacturer,
                                            sorted_by_type[q].price, sorted_by_type[q].service_date,
                                            sorted_by_type[q].damaged])
+
+    # c. PastServiceDateInventory.csv – all the items that are past the service date on the day
+    # the program is actually executed. Each row should contain: item ID, manufacturer
+    # name, item type, price, service date, and list if it is damaged. The items must appear in
+    # the order of service date from oldest to most recent.
 
     print_tester(master_item_list)
     print_tester(sorted_by_manufacturer)
