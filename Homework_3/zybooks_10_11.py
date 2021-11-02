adcd1f6ebd2f7c7c274d36039ba0882271a89e7d
helmutkbrenner <66104915+helmutkbrenner@users.noreply.github.com>
2021-11-02 14:08:34 -0500

    Not sure if we are supposed to use the provided code, but I added a constructor and started by getting the user input required. This question is worded oddly.
    Signed-off-by: helmutkbrenner <66104915+helmutkbrenner@users.noreply.github.com>

--- Homework_3/zybooks_10_11.py
+++ Homework_3/zybooks_10_11.py
@@ -1,2 +1,31 @@
 #  Helmut Brenner  #
 #  2037275 #
+
+class FoodItem:
+    def __init__(self, name='None', fat=0, carbs=0, protein=0):
+        self.name = name
+        self.fat = fat
+        self.carbs = carbs
+        self.protein = protein
+
+
+def get_calories(self, num_servings):
+    # Calorie formula
+    calories = ((self.fat * 9) + (self.carbs * 4) + (self.protein * 4)) * num_servings
+    return calories
+
+
+def print_info(self):
+    print('Nutritional information per serving of {}:'.format(self.name))
+    print('   Fat: {:.2f} g'.format(self.fat))
+    print('   Carbohydrates: {:.2f} g'.format(self.carbs))
+    print('   Protein: {:.2f} g'.format(self.protein))
+
+
+if __name__ == '__main__':
+    # Get user inputs
+    user_input = []
+    for i in range(5):
+        user_input.append(input())
+    print(user_input)
+
