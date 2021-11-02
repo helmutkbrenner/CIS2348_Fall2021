eaf75a9ddc22998ddb63ab88b11ec1c8d64ac9f9
helmutkbrenner <66104915+helmutkbrenner@users.noreply.github.com>
2021-11-02 15:26:08 -0500

    okay, this one was pretty easy, but I still need to run it through the grader. just had to define get_win_percentage and write the main program.
    Signed-off-by: helmutkbrenner <66104915+helmutkbrenner@users.noreply.github.com>

--- Homework_3/zybooks_10_15.py
+++ Homework_3/zybooks_10_15.py
@@ -1,2 +1,27 @@
 #  Helmut Brenner  #
 #  2037275  #
+
+class Team:
+    def __init__(self):
+        self.team_name = 'none'
+        self.team_wins = 0
+        self.team_losses = 0
+
+    def get_win_percentage(self):
+        win_percentage = float(self.team_wins) / (self.team_wins + self.team_losses)
+        return win_percentage
+
+
+if __name__ == '__main__':
+    team = Team()
+
+    team.team_name = str(input())
+    team.team_wins = float(input())
+    team.team_losses = float(input())
+
+    win_percent = team.get_win_percentage()
+
+    if win_percent >= 0.5:
+        print('Congratulations, Team {} has a winning average!'.format(team.team_name))
+    else:
+        print('Team {} has a losing average.'.format(team.team_name))
