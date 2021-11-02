40fdaccb252ca2ae443a9c5bd398ace8807d2dcf
helmutkbrenner <66104915+helmutkbrenner@users.noreply.github.com>
2021-11-02 15:57:47 -0500

    okay, got some errors in my spacing I am trying to fix and maybe some decimal point issues.
    Signed-off-by: helmutkbrenner <66104915+helmutkbrenner@users.noreply.github.com>

--- Homework_3/zybooks_10_17.py
+++ Homework_3/zybooks_10_17.py
@@ -1,2 +1,34 @@
 #  Helmut Brenner  #
 #  2037275  #
+
+class ItemToPurchase:
+    def __init__(self, item_name='none', item_price=0, item_quantity=0):
+        self.item_name = item_name
+        self.item_price = item_price
+        self.item_quantity = item_quantity
+
+    def print_item_cost(self):
+        print('{} {} @ ${} = {}'.format(self.item_name, self.item_quantity, self.item_price,
+                                        (self.item_price * self.item_quantity)))
+
+
+if __name__ == '__main__':
+    item1 = ItemToPurchase()
+    item2 = ItemToPurchase()
+
+    print('Item 1')
+    item1.item_name = str(input('Enter the item name:\n'))
+    item1.item_price = float(input('Enter the item price:\n'))
+    item1.item_quantity = int(input('Enter the item quantity\n'))
+
+    print('Item 2')
+    item2.item_name = str(input('Enter the item name:\n'))
+    item2.item_price = float(input('Enter the item price:\n'))
+    item2.item_quantity = int(input('Enter the item quantity:\n'))
+
+    print('\nTOTAL COST')
+    item1.print_item_cost()
+    item2.print_item_cost()
+
+    print('Total: ${}'.format((item1.item_price * item1.item_quantity) + (item2.item_price * item2.item_quantity))
+
