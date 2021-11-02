868c5b72e5624e0fb569acf410b8328bc2d67f1f
helmutkbrenner <66104915+helmutkbrenner@users.noreply.github.com>
2021-11-02 14:31:09 -0500

    fixed the instance methods so they would work in main(). Ran into some typing issues so I cast the necessary list elements with int(). Also experimenting with printing the output.
    Signed-off-by: helmutkbrenner <66104915+helmutkbrenner@users.noreply.github.com>

--- Homework_3/zybooks_10_11.py
+++ Homework_3/zybooks_10_11.py
@@ -2,24 +2,22 @@
 #  2037275 #
 
 class FoodItem:
-    def __init__(self, name='None', fat=0, carbs=0, protein=0):
+    def __init__(self, name='None', fat=0.0, carbs=0.0, protein=0.0):
         self.name = name
         self.fat = fat
         self.carbs = carbs
         self.protein = protein
 
+    def get_calories(self, num_servings):
+        # Calorie formula
+        calories = ((self.fat * 9) + (self.carbs * 4) + (self.protein * 4)) * num_servings
+        return calories
 
-def get_calories(self, num_servings):
-    # Calorie formula
-    calories = ((self.fat * 9) + (self.carbs * 4) + (self.protein * 4)) * num_servings
-    return calories
-
-
-def print_info(self):
-    print('Nutritional information per serving of {}:'.format(self.name))
-    print('   Fat: {:.2f} g'.format(self.fat))
-    print('   Carbohydrates: {:.2f} g'.format(self.carbs))
-    print('   Protein: {:.2f} g'.format(self.protein))
+    def print_info(self):
+        print('Nutritional information per serving of {}:'.format(self.name))
+        print('   Fat: {:.2f} g'.format(self.fat))
+        print('   Carbohydrates: {:.2f} g'.format(self.carbs))
+        print('   Protein: {:.2f} g'.format(self.protein))
 
 
 if __name__ == '__main__':
@@ -27,5 +25,10 @@ if __name__ == '__main__':
     user_input = []
     for i in range(5):
         user_input.append(input())
-    print(user_input)
 
+    food1 = FoodItem(user_input[0], int(user_input[1]), int(user_input[2]), int(user_input[3]))
+    cal = food1.get_calories(int(user_input[4]))
+    print(cal)
+
+    food1.print_info()
+    print('Number of calories for {} serving(s): {}'.format(user_input[4], cal))
