d0b7a2f802c07323e7874f353a86d4ceb26b133f
helmutkbrenner <66104915+helmutkbrenner@users.noreply.github.com>
2021-11-08 14:19:45 -0600

    should be a pretty easy sort, i could also do this using built in methods i suppose
    Signed-off-by: helmutkbrenner <66104915+helmutkbrenner@users.noreply.github.com>

--- Homework_3/zybooks_11_18.py
+++ Homework_3/zybooks_11_18.py
@@ -1,2 +1,11 @@
 #  Helmut Brenner  #
 #  2037275  #
+
+if __name__ == '__main__':
+    master_list = input()
+    answer_list = []
+    for i in range(len(master_list)):
+        if master_list[i] > 0:
+            if master_list[i] >= answer_list[-1]:
+                answer_list.append(master_list[i])
+    print(answer_list)
