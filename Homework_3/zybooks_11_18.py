44aaa6f585841516bc918ad4adbf97ee2c454d22
helmutkbrenner <66104915+helmutkbrenner@users.noreply.github.com>
2021-11-08 14:44:33 -0600

    It was difficult because I did not know how to enter my inputs in the terminal as lists...
    Signed-off-by: helmutkbrenner <66104915+helmutkbrenner@users.noreply.github.com>

--- Homework_3/zybooks_11_18.py
+++ Homework_3/zybooks_11_18.py
@@ -2,10 +2,10 @@
 #  2037275  #
 
 if __name__ == '__main__':
-    master_list = input()
-    answer_list = []
-    for i in range(len(master_list)):
-        if master_list[i] > 0:
-            if master_list[i] >= answer_list[-1]:
-                answer_list.append(master_list[i])
-    print(answer_list)
+    master_list = [int(x) for x in input().split()]
+
+    unsorted_list = [i for i in master_list if i >= 0]
+    sorted_list = sorted(unsorted_list)
+
+    for i in range(len(sorted_list)):
+        print(sorted_list[i], end=' ')
