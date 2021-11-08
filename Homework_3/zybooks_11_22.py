826e219e8248e947dd7fd084ff071f3188687f34
helmutkbrenner <66104915+helmutkbrenner@users.noreply.github.com>
2021-11-08 15:21:51 -0600

    Easiest one of the whole-lot.
    Signed-off-by: helmutkbrenner <66104915+helmutkbrenner@users.noreply.github.com>

--- Homework_3/zybooks_11_22.py
+++ Homework_3/zybooks_11_22.py
@@ -2,3 +2,17 @@
 #  2037275  #
 
 if __name__ == '__main__':
+    #  recycled code from the last problem but changed for strings.
+    master_list = [str(x) for x in input().split()]
+    my_dictionary = {}
+
+    #  Loads the dictionary
+    for i in master_list:
+        if i not in my_dictionary:
+            my_dictionary[i] = 1
+        else:
+            my_dictionary[i] += 1
+
+    #  prints the words in order, with the number of times that word shows up in the whole string
+    for j in master_list:
+        print(j, my_dictionary[j])
