82ce115c3199d734e9fdf0fc8563b710f62d939d
helmutkbrenner <66104915+helmutkbrenner@users.noreply.github.com>
2021-11-08 15:53:26 -0600

    pretty good so far, got all the numbers as input, stored them to a dictionary and confirmed it with a test print. Dont really know if I will use the key_list or not...
    Signed-off-by: helmutkbrenner <66104915+helmutkbrenner@users.noreply.github.com>

--- Homework_3/zybooks_11_27.py
+++ Homework_3/zybooks_11_27.py
@@ -1,2 +1,17 @@
 #  Helmut Brenner  #
 #  2037275  #
+
+if __name__ == '__main__':
+    my_dictionary = {}
+    key_list = []
+
+    for i in range(1, 6):
+        print('Enter player {}\'s jersey number:'.format(i))
+        jersey_number = int(input())
+        print('Enter player {}\'s rating:')
+        player_rating = int(input())
+        my_dictionary[jersey_number] = player_rating
+        key_list.append(jersey_number)
+
+    for i in my_dictionary:
+        print(i, my_dictionary[i])
