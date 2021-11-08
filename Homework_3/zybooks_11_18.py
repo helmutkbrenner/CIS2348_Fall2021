ef3f821537b650fa112af3db74e2053c79c75c2c
helmutkbrenner <66104915+helmutkbrenner@users.noreply.github.com>
2021-11-08 14:52:25 -0600

    notes
    Signed-off-by: helmutkbrenner <66104915+helmutkbrenner@users.noreply.github.com>

--- Homework_3/zybooks_11_18.py
+++ Homework_3/zybooks_11_18.py
@@ -2,10 +2,13 @@
 #  2037275  #
 
 if __name__ == '__main__':
+    #  take input as a list and turn it into the master list
     master_list = [int(x) for x in input().split()]
 
+    #  use a comprehension to make a new list with only non-negative values
     unsorted_list = [i for i in master_list if i >= 0]
     sorted_list = sorted(unsorted_list)
 
+    #  print values with spacing
     for i in range(len(sorted_list)):
         print(sorted_list[i], end=' ')
