e763c84892652efe140b6f3dd25d4ec454f6ac49
helmutkbrenner <66104915+helmutkbrenner@users.noreply.github.com>
2021-11-29 11:26:42 -0600

    put in starter code
    Signed-off-by: helmutkbrenner <66104915+helmutkbrenner@users.noreply.github.com>

--- Homework_4/zybooks_12_9.py
+++ Homework_4/zybooks_12_9.py
@@ -1,2 +1,16 @@
 #  Helmut Brenner  #
 #  2037275  #
+
+if __name__ == '__main__':
+    # Split input into 2 parts: name and age
+    parts = input().split()
+    name = parts[0]
+    while name != '-1':
+        # FIXME: The following line will throw ValueError exception.
+        #        Insert try/except blocks to catch the exception.
+        age = int(parts[1]) + 1
+        print('{} {}'.format(name, age))
+
+        # Get next line
+        parts = input().split()
+        name = parts[0]
