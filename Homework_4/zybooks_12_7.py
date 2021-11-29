025ed678d65ec7acde5a105a4efdc8576876a401
helmutkbrenner <66104915+helmutkbrenner@users.noreply.github.com>
2021-11-29 11:24:31 -0600

    changed a variable name to make pycharm happy and the rest is finished
    Signed-off-by: helmutkbrenner <66104915+helmutkbrenner@users.noreply.github.com>

--- Homework_4/zybooks_12_7.py
+++ Homework_4/zybooks_12_7.py
@@ -4,22 +4,20 @@
 def get_age():
     age = int(input())
     if age < 18 or age > 75:
-        raise ValueError('Invalid Age')
+        raise ValueError('Invalid age.')
     return age
 
 
 def fat_burning_heart_rate(age):
-    heart_rate = (220 * .7) - age
+    heart_rate = (220 - age) * .7
     return heart_rate
 
 
 if __name__ == "__main__":
-    # TODO: Modify to call get_age() and fat_burning_heart_rate()
-    #       and handle the exception
     try:
         age = get_age()
         burn_rate = fat_burning_heart_rate(age)
         print('Fat burning heart rate for a {} year-old: {} bpm'.format(age, burn_rate))
-    except ValueError as excpt:
+    except ValueError as expt:
         print(excpt)
-        print('Could not Calculate heart rate info.')
+        print('Could not calculate heart rate info.\n')
