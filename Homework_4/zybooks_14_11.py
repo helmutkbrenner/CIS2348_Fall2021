240720d842a79314f786fb52e9e96011346a5291
helmutkbrenner <66104915+helmutkbrenner@users.noreply.github.com>
2021-11-29 12:02:48 -0600

    has issues with 3 digit numbers?
    Signed-off-by: helmutkbrenner <66104915+helmutkbrenner@users.noreply.github.com>

--- Homework_4/zybooks_14_11.py
+++ Homework_4/zybooks_14_11.py
@@ -1,2 +1,20 @@
 #  Helmut Brenner  #
 #  2037275  #
+
+def selection_sort_descend_trace(integer_list):
+    for k in range(len(integer_list) - 1):
+        index_of_smallest = k
+        for j in range(k + 1, len(integer_list)):
+            if integer_list[j] > integer_list[index_of_smallest]:
+                index_of_smallest = j
+
+        number = integer_list[k]
+        integer_list[k] = integer_list[index_of_smallest]
+        integer_list[index_of_smallest] = number
+        print(' '.join(integer_list) + ' ')
+
+
+if __name__ == '__main__':
+    number_list = input()
+    list = number_list.split()
+    selection_sort_descend_trace(list)
