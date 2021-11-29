46a5357fffce0c1f0120b776e7c6b346f48448ee
helmutkbrenner <66104915+helmutkbrenner@users.noreply.github.com>
2021-11-29 11:32:09 -0600

    easy peasy
    Signed-off-by: helmutkbrenner <66104915+helmutkbrenner@users.noreply.github.com>

--- Homework_4/zybooks_12_9.py
+++ Homework_4/zybooks_12_9.py
@@ -8,9 +8,12 @@ if __name__ == '__main__':
     while name != '-1':
         # FIXME: The following line will throw ValueError exception.
         #        Insert try/except blocks to catch the exception.
-        age = int(parts[1]) + 1
-        print('{} {}'.format(name, age))
-
+        try:
+            age = int(parts[1]) + 1
+            print('{} {}'.format(name, age))
+        except ValueError as exception:
+            age = 0
+            print('{} {}'.format(name, age))
         # Get next line
         parts = input().split()
         name = parts[0]
