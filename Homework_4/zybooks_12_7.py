dff5ad3edd78bfa6f130cb5c7c3990427334aa48
helmutkbrenner <66104915+helmutkbrenner@users.noreply.github.com>
2021-11-29 11:20:18 -0600

    Got the starter code in here and then raise an exception. Gonna still run it through the grader and for sure fix some things
    Signed-off-by: helmutkbrenner <66104915+helmutkbrenner@users.noreply.github.com>

--- Homework_4/zybooks_12_7.py
+++ Homework_4/zybooks_12_7.py
@@ -1,3 +1,25 @@
 #  Helmut Brenner  #
 #  2037275  #
 
+def get_age():
+    age = int(input())
+    if age < 18 or age > 75:
+        raise ValueError('Invalid Age')
+    return age
+
+
+def fat_burning_heart_rate(age):
+    heart_rate = (220 * .7) - age
+    return heart_rate
+
+
+if __name__ == "__main__":
+    # TODO: Modify to call get_age() and fat_burning_heart_rate()
+    #       and handle the exception
+    try:
+        age = get_age()
+        burn_rate = fat_burning_heart_rate(age)
+        print('Fat burning heart rate for a {} year-old: {} bpm'.format(age, burn_rate))
+    except ValueError as excpt:
+        print(excpt)
+        print('Could not Calculate heart rate info.')
