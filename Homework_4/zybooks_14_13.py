8cc059364f8fcf1fcdb3b5adc158155a72694c0e
helmutkbrenner <66104915+helmutkbrenner@users.noreply.github.com>
2021-11-29 13:13:53 -0600

    I think the partitioning algo works as required.... just following along in the textbook
    Signed-off-by: helmutkbrenner <66104915+helmutkbrenner@users.noreply.github.com>

--- Homework_4/zybooks_14_13.py
+++ Homework_4/zybooks_14_13.py
@@ -10,7 +10,28 @@ num_calls = 0
 #       initialized to the left and right sides of the current elements being sorted,
 #       and determine if a swap is necessary
 def partition(user_ids, i, k):
+    median = i + (k - i) // 2
+    pivot_value = user_ids[median]
 
+    l = i
+    h = k
+    finished = False
+
+    while not finished:
+        while user_ids[l] < pivot_value:
+            l += 1
+        while pivot_value < user_ids[h]:
+            h -= 1
+        if l >= h:
+            finished = True
+        else:
+            #  swap numbers
+            tmp = user_ids[l]
+            user_ids[l] = user_ids[h]
+            user_ids[h] = tmp
+            l += 1
+            h -= 1
+    return h
 
 # TODO: Write the quicksort algorithm that recursively sorts the low and
 #       high partitions. Add 1 to num_calls each time quisksort() is called
