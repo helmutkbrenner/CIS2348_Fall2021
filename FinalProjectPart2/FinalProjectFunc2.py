8caa9edde0e22f6b1c2b12d9b881a1103ca6a5a2
helmutkbrenner <66104915+helmutkbrenner@users.noreply.github.com>
2021-11-30 14:50:16 -0600

    old code imported and works as necessary
    Signed-off-by: helmutkbrenner <66104915+helmutkbrenner@users.noreply.github.com>

--- FinalProjectPart2/FinalProjectFunc2.py
+++ FinalProjectPart2/FinalProjectFunc2.py
@@ -1,2 +1,57 @@
 #  Helmut Brenner  #
 #  2037275  #
+
+class Item:
+    #  Making an item class with the following attributes and methods:
+    def __init__(self, item_id, manufacturer, item_type, damaged='undamaged', price=0, service_date=''):
+        self.item_id = item_id
+        self.manufacturer = manufacturer
+        self.item_type = item_type
+        self.damaged = damaged
+        self.price = price
+        self.service_date = service_date
+
+
+def add_prices(master_list, price_pairs):
+    # This function is used to take the list of class objects and "append" the price onto the correct item entry. Upon
+    #  second looks, I know these search algos are NOT efficient, maybe room for improvement on part 2 of the project?
+    for k in range(len(master_list)):
+        j = 0
+        while master_list[k].item_id != price_pairs[j][0]:
+            j += 1
+        master_list[k].price = int(price_pairs[j][1])
+    return master_list
+
+
+def add_service_dates(master_list, service_dates):
+    #  Basic same functionality as the add_prices function above.
+    for k in range(len(master_list)):
+        j = 0
+        while master_list[k].item_id != service_dates[j][0]:
+            j += 1
+        master_list[k].service_date = service_dates[j][1]
+    return master_list
+
+
+def print_tester(master_list):
+    for h in range(len(master_list)):
+        #  TEST  function that was used to verify the sorting of the class object lists
+        print(master_list[h].item_id, master_list[h].manufacturer, master_list[h].item_type,
+              master_list[h].damaged, master_list[h].price, master_list[h].service_date)
+    print('\n')
+
+
+def get_item_type_list(class_list_by_type):
+    #  Gets a list of all the item types, without repeats for reference in the main program.
+    list_of_types = []
+    for p in range(len(class_list_by_type)):
+        if class_list_by_type[p].item_type not in list_of_types:
+            list_of_types.append(class_list_by_type[p].item_type)
+    return list_of_types
+
+
+def file_name_formatter(type_list):
+    # List comprehension is POG
+    from string import capwords
+    formatted_list = [type.capitalize() + 'Inventory.csv' for type in type_list]
+    return formatted_list
--- FinalProjectPart2/FinalProjectMain2.py
+++ FinalProjectPart2/FinalProjectMain2.py
@@ -1,2 +1,112 @@
 #  Helmut Brenner  #
 #  2037275  #
+
+import csv
+import string
+import FinalProjectFunc2
+
+
+if __name__ == '__main__':
+    with open('ManufacturerList.csv', 'r') as manufacturer_file:
+        #  Opens the file and writes the contents to our class constructor Item, outputs to a master_item_list
+        line_reader = csv.reader(manufacturer_file, delimiter=',')
+        line_count = 0
+        master_item_list = []
+        for row in line_reader:
+            master_item_list.append(FinalProjectFunc2.Item(row[0], row[1], row[2], row[3]))
+            line_count += 1
+
+    with open('PriceList.csv', 'r') as price_list_file:
+        #  Opens the Price list and saves the contents as a list of lists. The list is utilized in the add prices func.
+        line_reader1 = csv.reader(price_list_file, delimiter=',')
+        line_count1 = 0
+        list_of_prices = []
+        for row in line_reader1:
+            list_of_prices.append([row[0], row[1]])
+            line_count1 += 1
+
+    with open('ServiceDatesList.csv', 'r') as service_dates_list_file:
+        #  Opens service dates list and saves the service dates list as a list of lists similar to before
+        line_reader2 = csv.reader(service_dates_list_file, delimiter=',')
+        line_count2 = 0
+        service_dates_list = []
+        for row in line_reader2:
+            service_dates_list.append([row[0], row[1]])
+            line_count2 += 1
+
+    #  Adds the prices to the correct items in their respective object classes
+    FinalProjectFunc2.add_prices(master_item_list, list_of_prices)
+
+    #  Adds the service dates to the correct items in their respective object classes
+    FinalProjectFunc2.add_service_dates(master_item_list, service_dates_list)
+
+    #  This bit of code imports a special Operator function to sort the lists according to common attributes
+    #  It even allows me to do two tier sorting, AMAZING.
+
+    from operator import attrgetter
+    sorted_by_manufacturer = sorted(master_item_list, key=attrgetter('manufacturer'))
+    sorted_by_id = sorted(master_item_list, key=attrgetter('item_id'))
+    sorted_by_type = sorted(master_item_list, key=attrgetter('item_type', 'item_id'))
+    sorted_by_price = sorted(master_item_list, key=attrgetter('price'), reverse=True)
+
+    with open('FullInventory.csv', 'w') as full_inventory:
+        #  This piece of code creates the file FullInventory and writes the data in the required order.
+        line_writer = csv.writer(full_inventory)
+
+        for i in range(len(sorted_by_manufacturer)):
+            line_writer.writerow([sorted_by_manufacturer[i].item_id, sorted_by_manufacturer[i].manufacturer,
+                                  sorted_by_manufacturer[i].item_type, sorted_by_manufacturer[i].price,
+                                  sorted_by_manufacturer[i].service_date, sorted_by_manufacturer[i].damaged])
+
+    #  One list provides the formatting names for the file names, and the other provides a reference for all types
+    #  encountered in the input files. Allows for scalability for any number of item types... Theoretically.
+
+    ref_list_item_types = FinalProjectFunc2.get_item_type_list(sorted_by_type)
+    formatted_file_names = FinalProjectFunc2.file_name_formatter(ref_list_item_types)
+
+    for i in range(len(formatted_file_names)):
+        # This piece of code takes a list of formatted file names and makes the appropriate files from the appropriate
+        # data
+        with open(formatted_file_names[i], 'w') as type_file:
+            line_writer2 = csv.writer(type_file)
+            for q in range(len(sorted_by_type)):
+                if sorted_by_type[q].item_type == ref_list_item_types[i]:
+                    line_writer2.writerow([sorted_by_type[q].item_id, sorted_by_type[q].manufacturer,
+                                           sorted_by_type[q].price, sorted_by_type[q].service_date,
+                                           sorted_by_type[q].damaged])
+
+    # This piece of code will scan the sorted class objects by service date and add them to a list if they are past due.
+    # It will also add a class object to the list if the service date and current date are the same.
+
+    from datetime import datetime
+    today_date = datetime.now()
+    past_serv_date_list = []
+
+    #  This piece of code checks to make sure that the service date is before today's date and transforms the date into
+    #  a datetime object.
+    for m in range(len(sorted_by_price)):
+        sorted_by_price[m].service_date = datetime.strptime(sorted_by_price[m].service_date, '%m/%d/%Y')
+        if sorted_by_price[m].service_date < today_date:
+            past_serv_date_list.append(sorted_by_price[m])
+
+    # Sort the list of class objects by date, once the dates are datetime objects the comparison is done by sorted()
+    sorted_dates_list = sorted(past_serv_date_list, key=attrgetter('service_date'))
+
+    # Here we use a csv writer to write the appropriate attributes from the list of class objects we just sorted above.
+    # I used the datetime formatting codes to output the correctly formatted dates to the csv file.
+    with open('PastServiceDateInventory.csv', 'w') as service_dates_file:
+        line_writer3 = csv.writer(service_dates_file)
+        for o in range(len(sorted_dates_list)):
+            line_writer3.writerow([sorted_dates_list[o].item_id, sorted_dates_list[o].manufacturer,
+                                   sorted_dates_list[o].item_type, sorted_dates_list[o].price,
+                                   sorted_dates_list[o].service_date.strftime('%m/%d/%Y'),
+                                   sorted_dates_list[o].damaged])
+
+    # This is where we find damaged items and write them to their own file.
+    with open('DamagedInventory.csv', 'w') as damaged_items_file:
+        line_writer4 = csv.writer(damaged_items_file)
+        for o in range(len(sorted_by_price)):
+            if sorted_by_price[o].damaged != '':
+                line_writer4.writerow([sorted_by_price[o].item_id, sorted_by_price[o].manufacturer,
+                                       sorted_by_price[o].item_type, sorted_by_price[o].price,
+                                       sorted_by_price[o].service_date.strftime('%m/%d/%Y')])
